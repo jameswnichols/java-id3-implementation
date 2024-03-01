784c6d7d47b67a807332759b3d53e9af616a87f7
James Nichols <jnichols.tigley@gmail.com>
2024-03-01 15:10:37 +0000

    More main changes.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args){
+        float startTime = System.currentTimeMillis();
         Dataset dataset = new Dataset("courseworkDataset.csv");
         Node rootNode = dataset.getTreeRootNode();
         rootNode.renderTree();
+        float elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println("Finished in "+elapsedTime+"ms.");
     }
 }
